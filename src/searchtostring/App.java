679c2597a032879895a86e5038a68d9f3ca05432
Edvin Kaapa <edvin.kaapa@ivkhk.ee>
2020-09-23 11:22:42 +0300

    rabota gotova

--- src/searchtostring/App.java
+++ src/searchtostring/App.java
@@ -5,10 +5,25 @@
  */
 package searchtostring;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 class App {
+    public void run() {
+        System.out.println("Поиски в строчке");
+        System.out.println("введите текстс");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("введите строку:");
+        String searchStr = scanner.nextLine();
+        int i = 0;
+        
+        System.out.println("Искомая строка начинается на %d позиции%, i");
+    
+    
+    }
     
 }
